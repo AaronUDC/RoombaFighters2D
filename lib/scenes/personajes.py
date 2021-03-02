19e88000adcb5578f87c2130aa6416ac0a8ac211
carlos <carlos.suarez.fernandez@udc.es>
2021-03-02 21:47:34 +0100

    Personajes.py creado y casi completo.

---
+++ lib/scenes/personajes.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+
+import pygame, sys, os
+from pygame.locals import *
+from escena import *
+from gestorRecursos import *
+
+# -------------------------------------------------
+# -------------------------------------------------
+# Constantes
+# -------------------------------------------------
+# -------------------------------------------------
+
+# Movimientos
+QUIETO = 0
+IZQUIERDA = 1
+DERECHA = 2
+ARRIBA = 3
+ABAJO = 4
+
+
+VELOCIDAD_JUGADOR = 0.2 # Pixeles por milisegundo
+
+class MiSprite(pygame.sprite.Sprite):
+    "Los Sprites que tendra este juego"
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.posicion = (0, 0)
+        self.velocidad = (0, 0)
+        self.scroll   = (0, 0)
+
+    def establecerPosicion(self, posicion):
+        self.posicion = posicion
+        self.rect.left = self.posicion[0] - self.scroll[0]
+        self.rect.bottom = self.posicion[1] - self.scroll[1]
+
+    def establecerPosicionPantalla(self, scrollDecorado):
+        self.scroll = scrollDecorado;
+        (scrollx, scrolly) = self.scroll;
+        (posx, posy) = self.posicion;
+        self.rect.left = posx - scrollx;
+        self.rect.bottom = posy - scrolly;
+
+    def incrementarPosicion(self, incremento):
+        (posx, posy) = self.posicion
+        (incrementox, incrementoy) = incremento
+        self.establecerPosicion((posx+incrementox, posy+incrementoy))
+
+    def update(self, tiempo):
+        incrementox = self.velocidad[0]*tiempo
+        incrementoy = self.velocidad[1]*tiempo
+        self.incrementarPosicion((incrementox, incrementoy))
+
+
+
+# -------------------------------------------------
+# Clases Personaje
+
+#class Personaje(pygame.sprite.Sprite):
+class Personaje(MiSprite):
+    "Cualquier personaje del juego"
+
+    # Parametros pasados al constructor de esta clase:
+    #  Archivo con la hoja de Sprites
+    #  Archivo con las coordenadoas dentro de la hoja
+    #  Numero de imagenes en cada postura
+    #  Velocidad de caminar 
+    #  Retardo para mostrar la animacion del personaje
+    def __init__(self, archivoImagen, archivoCoordenadas, numImagenes, velocidadCarrera, retardoAnimacion):
+
+        # Primero invocamos al constructor de la clase padre
+        MiSprite.__init__(self);
+
+        # Se carga la hoja
+        self.hoja = GestorRecursos.CargarImagen(archivoImagen,-1)
+        self.hoja = self.hoja.convert_alpha()
+        # El movimiento que esta realizando
+        self.movimiento = QUIETO
+        # Lado hacia el que esta mirando
+        self.mirando = IZQUIERDA
+
+        # Leemos las coordenadas de un archivo de texto
+        datos = GestorRecursos.CargarArchivoCoordenadas(archivoCoordenadas)
+        datos = datos.split()
+        self.numPostura = 1;
+        self.numImagenPostura = 0;
+        cont = 0;
+        self.coordenadasHoja = [];
+        for linea in range(0, 3):
+            self.coordenadasHoja.append([])
+            tmp = self.coordenadasHoja[linea]
+            for postura in range(1, numImagenes[linea]+1):
+                tmp.append(pygame.Rect((int(datos[cont]), int(datos[cont+1])), (int(datos[cont+2]), int(datos[cont+3]))))
+                cont += 4
+
+        # El retardo a la hora de cambiar la imagen del Sprite (para que no se mueva demasiado rápido)
+        self.retardoMovimiento = 0;
+
+        # En que postura esta inicialmente
+        self.numPostura = QUIETO
+
+        # El rectangulo del Sprite
+        self.rect = pygame.Rect(100,100,self.coordenadasHoja[self.numPostura][self.numImagenPostura][2],self.coordenadasHoja[self.numPostura][self.numImagenPostura][3])
+
+        # Las velocidades de caminar
+        self.velocidadCarrera = velocidadCarrera
+
+        # El retardo en la animacion del personaje (podria y deberia ser distinto para cada postura)
+        self.retardoAnimacion = retardoAnimacion
+
+        # Y actualizamos la postura del Sprite inicial, llamando al metodo correspondiente
+        self.actualizarPostura()
+
+
+    # Metodo base para realizar el movimiento: simplemente se le indica cual va a hacer, y lo almacena
+    def mover(self, movimiento):
+        self.movimiento = movimiento
+
+
+    def actualizarPostura(self):
+        self.retardoMovimiento -= 1
+        # Miramos si ha pasado el retardo para dibujar una nueva postura
+        if (self.retardoMovimiento < 0):
+            self.retardoMovimiento = self.retardoAnimacion
+            # Si ha pasado, actualizamos la postura
+            self.numImagenPostura += 1
+            if self.numImagenPostura >= len(self.coordenadasHoja[self.numPostura]):
+                self.numImagenPostura = 0;
+            if self.numImagenPostura < 0:
+                self.numImagenPostura = len(self.coordenadasHoja[self.numPostura])-1
+            self.image = self.hoja.subsurface(self.coordenadasHoja[self.numPostura][self.numImagenPostura])
+
+            #OJOOOO ESTO DE AQUI NO SE COMO TRATARLO PARA PONER LAS 4 POSICIONES DE LA ROMBA
+            # Si esta mirando a la izquiera, cogemos la porcion de la hoja
+            if self.mirando == IZQUIERDA:
+                self.image = self.hoja.subsurface(self.coordenadasHoja[self.numPostura][self.numImagenPostura])
+            #  Si no, si mira a la derecha, invertimos esa imagen
+            elif self.mirando == DERECHA:
+                self.image = pygame.transform.flip(self.hoja.subsurface(self.coordenadasHoja[self.numPostura][self.numImagenPostura]), 1, 0)
+
+
+    def update(self, grupoPlataformas, tiempo):
+
+        # Las velocidades a las que iba hasta este momento
+        (velocidadx, velocidady) = self.velocidad
+
+        # Si vamos a la izquierda o a la derecha        
+        if (self.movimiento == IZQUIERDA) or (self.movimiento == DERECHA) or (self.movimiento == ARRIBA) or (self.movimiento == ABAJO):
+            # Esta mirando hacia ese lado
+            self.mirando = self.movimiento
+
+            # Si vamos a la izquierda, le ponemos velocidad en esa dirección
+            if self.movimiento == IZQUIERDA:
+                velocidadx = -self.velocidadCarrera
+            # Si vamos a la arriba, le ponemos velocidad en esa dirección
+            if self.movimiento == ARRIBA:
+                velocidady = -self.velocidadCarrera
+            # Si vamos a la derecha, le ponemos velocidad en esa dirección
+            else:
+                velocidadx = self.velocidadCarrera
+            # Si vamos a la abajo, le ponemos velocidad en esa dirección
+            else:
+                velocidady = self.velocidadCarrera
+
+        # Si no se ha pulsado ninguna tecla
+        elif self.movimiento == QUIETO:
+            self.numPostura = SPRITE_QUIETO
+            velocidadx = 0
+
+
+        # Actualizamos la imagen a mostrar
+        self.actualizarPostura()
+
+        # Aplicamos la velocidad en cada eje      
+        self.velocidad = (velocidadx, velocidady)
+
+        # Y llamamos al método de la superclase para que, según la velocidad y el tiempo
+        #  calcule la nueva posición del Sprite
+        MiSprite.update(self, tiempo)
+        
+        return
