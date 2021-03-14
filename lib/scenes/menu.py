df5f59952fd55a94389a328eb214a95ad48cb399
Aarón García Cerqueiras <a.garcia7@udc.es>
2021-03-14 20:06:40 +0100

    He empezado el tema del menú, se puede ir a la fase del salón, y salir del juego, tengo que retocar el aspecto, obviamente pero ya tenemos la base

--- lib/scenes/menu.py
+++ lib/scenes/menu.py
@@ -4,6 +4,134 @@ import pygame
 # import pyganim
 from pygame.locals import *
 from lib.escena import *
+from lib.scenes.salon import Salon
+from lib.scenes.test import Test
+from lib.gestorRecursos import *
+
+# Elementos UI, luego lo pondré en clases separadas
+class ElementoGUI:
+    def __init__(self, pantalla, rectangulo):
+        self.pantalla = pantalla
+        self.rect = rectangulo
+
+    def establecerPosicion(self, posicion):
+        (posicionx, posiciony) = posicion
+        self.rect.left = posicionx
+        self.rect.bottom = posiciony
+
+    def posicionEnElemento(self, posicion):
+        (posicionx, posiciony) = posicion
+        if (posicionx>=self.rect.left) and (posicionx<=self.rect.right) and (posiciony>=self.rect.top) and (posiciony<=self.rect.bottom):
+            return True
+        else:
+            return False
+
+    def dibujar(self):
+        raise NotImplemented("Tiene que implementar el metodo dibujar.")
+    def accion(self):
+        raise NotImplemented("Tiene que implementar el metodo accion.")
+
+class Boton(ElementoGUI):
+    def __init__(self, pantalla, nombreImagen, posicion):
+        # Se carga la imagen del boton
+        self.imagen = GestorRecursos.CargarImagen(nombreImagen,-1)
+        #self.imagen = pygame.transform.scale(self.imagen, (, 20))
+        # Se llama al método de la clase padre con el rectángulo que ocupa el botón
+        ElementoGUI.__init__(self, pantalla, self.imagen.get_rect())
+        # Se coloca el rectangulo en su posicion
+        self.establecerPosicion(posicion)
+    def dibujar(self, pantalla):
+        pantalla.blit(self.imagen, self.rect)
+
+class BotonJugarFase(Boton):
+    def __init__(self, pantalla, fase, posicion):
+        Boton.__init__(self, pantalla, 'botonGUI.png', posicion)
+        self.fase = fase
+    def accion(self):
+        self.pantalla.menu.ejecutarJuego(self.fase)
+
+# -------------------------------------------------
+# Clase TextoGUI y los distintos textos
+
+class TextoGUI(ElementoGUI):
+    def __init__(self, pantalla, fuente, color, texto, posicion):
+        # Se crea la imagen del texto
+        self.imagen = fuente.render(texto, True, color)
+        # Se llama al método de la clase padre con el rectángulo que ocupa el texto
+        ElementoGUI.__init__(self, pantalla, self.imagen.get_rect())
+        # Se coloca el rectangulo en su posicion
+        self.establecerPosicion(posicion)
+    def dibujar(self, pantalla):
+        pantalla.blit(self.imagen, self.rect)
+
+class TextoJugarFase(TextoGUI):
+    def __init__(self, pantalla, texto, fase, posicion):
+        # La fuente la debería cargar el estor de recursos
+        fuente = pygame.font.Font('fuenteMenu.ttf', 26);
+        TextoGUI.__init__(self, pantalla, fuente, (0, 0, 0), texto,  posicion)
+        self.fase = fase
+    def accion(self):
+        self.pantalla.menu.ejecutarJuego(self.fase)
+
+class TextoSalir(TextoGUI):
+    def __init__(self, pantalla, posicion):
+        # La fuente la debería cargar el estor de recursos
+        fuente = pygame.font.Font('fuenteMenu.ttf', 26);
+        TextoGUI.__init__(self, pantalla, fuente, (0, 0, 0), 'Salir', posicion)
+    def accion(self):
+        self.pantalla.menu.salirPrograma()
+
+# -------------------------------------------------
+# Clase PantallaGUI y las distintas pantallas
+
+class PantallaGUI:
+    def __init__(self, menu, nombreImagen):
+        self.menu = menu
+        # Se carga la imagen de fondo
+        self.imagen = GestorRecursos.CargarImagen(nombreImagen)
+        self.imagen = pygame.transform.scale(self.imagen, (ANCHO_PANTALLA, ALTO_PANTALLA))
+        # Se tiene una lista de elementos GUI
+        self.elementosGUI = []
+        # Se tiene una lista de animaciones
+        self.animaciones = []
+
+    def eventos(self, lista_eventos):
+        for evento in lista_eventos:
+            if evento.type == MOUSEBUTTONDOWN:
+                self.elementoClic = None
+                for elemento in self.elementosGUI:
+                    if elemento.posicionEnElemento(evento.pos):
+                        self.elementoClic = elemento
+            if evento.type == MOUSEBUTTONUP:
+                for elemento in self.elementosGUI:
+                    if elemento.posicionEnElemento(evento.pos):
+                        if (elemento == self.elementoClic):
+                            elemento.accion()
+
+    def dibujar(self, pantalla):
+        # Dibujamos primero la imagen de fondo
+        pantalla.blit(self.imagen, self.imagen.get_rect())
+        # Después las animaciones
+        for animacion in self.animaciones:
+            animacion.dibujar(pantalla)
+        # Después los botones
+        for elemento in self.elementosGUI:
+            elemento.dibujar(pantalla)
+
+class PantallaInicialGUI(PantallaGUI):
+    def __init__(self, menu):
+        PantallaGUI.__init__(self, menu, 'suelo.png')
+        # Creamos los botones y los metemos en la lista
+        botonJugarFase1 = BotonJugarFase(self, Salon, (ANCHO_PANTALLA/2,ALTO_PANTALLA/2))
+        #botonSalir = BotonSalir(self)
+        self.elementosGUI.append(botonJugarFase1)
+        #self.elementosGUI.append(botonSalir)
+        # Creamos el texto y lo metemos en la lista
+        textoJugar = TextoJugarFase(self, 'Jugar en el salón', Salon, (ANCHO_PANTALLA/2+ 10,ALTO_PANTALLA/2-50))
+        textoSalir = TextoSalir(self, (ANCHO_PANTALLA/2,ALTO_PANTALLA/2+ 100))
+        self.elementosGUI.append(textoJugar)
+        self.elementosGUI.append(textoSalir)
+
 
 # -------------------------------------------------
 # Clase MenuPygame, la escena en sí, en Pygame
@@ -46,13 +174,13 @@ class Menu(EscenaPygame):
     def salirPrograma(self):
         self.director.salirPrograma()
 
-    # def ejecutarJuego(self):
-    #     # Creamos la escena con la animacion antes de jugar
-    #     escena = EscenaAnimacion(self.director)
-    #     self.director.apilarEscena(escena)
+    def ejecutarJuego(self, nivel):
+        # Creamos la escena con la animacion antes de jugar
+        pantalla = nivel(self.director)
+        self.director.apilarEscena(pantalla)
 
-    # def mostrarPantallaInicial(self):
-    #     self.pantallaActual = 0
+    def mostrarPantallaInicial(self):
+        self.pantallaActual = 0
 
-    # def mostrarPantallaConfiguracion(self):
-    #    self.pantallaActual = ...
+    #def mostrarPantallaConfiguracion(self):
+    #   self.pantallaActual = ...
